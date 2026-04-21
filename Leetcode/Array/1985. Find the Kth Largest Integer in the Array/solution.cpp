4aeba920de6b517f0cef8a0f03536bb9ea9ffbf0
adesh-singh596 <adeshsingh05906@gmail.com>
2026-04-21 22:45:30 +0530

    Update solution.cpp

--- Leetcode/Array/1985. Find the Kth Largest Integer in the Array/solution.cpp
+++ Leetcode/Array/1985. Find the Kth Largest Integer in the Array/solution.cpp
@@ -1,3 +1,13 @@
+/*
+Logic-->
+Constraint observation--> Length of element of the vector ie length of string upto 100==> can't convert to integer array and then solve it.
+Create a comparator function to sort the string vector by comparing the length of any two strings, if length are same then it will check character by character.
+
+New concept learnt-->
+1.Can customise the sort function as sort(nums.begin(),nums.end(),customise_fn)
+2.To convert an integer present in string vector into integer vector use stoi(ch) [string to integer]
+3.To add an element to a existing string ---> string_name+= to_string(c) 
+*/
 class Solution {
 public:
     static bool compa(const string& a,const string &b)
