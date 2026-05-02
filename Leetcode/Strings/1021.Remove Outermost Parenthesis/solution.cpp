6d25fcb4583d40376e3e3e9157ddbe49eda007a9
adesh-singh596 <adeshsingh05906@gmail.com>
2026-05-02 19:09:36 +0530

    Add function to remove outermost parentheses
    Implemented logic to remove outermost parentheses from a string by tracking depth.

--- Leetcode/Strings/1021.Remove Outermost Parenthesis/solution.cpp
+++ Leetcode/Strings/1021.Remove Outermost Parenthesis/solution.cpp
@@ -1,1 +1,30 @@
-// Solution
+// Approach--> 
+// Initialize depth that corresponds to how deep we are inside the parenthesis. (--> increases depth and )--> decreases depth.
+//If depth is less than 1 ie 0 , we will skip that character since it is outermost parenthesis, else add the character in string named answer
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int depth=0;
+        string ans;
+        for(auto ch:s)
+        {
+            if(ch=='(')
+            {
+                if(depth++>0)
+                {
+                    ans+=ch;
+                }
+                //depth++;
+            }
+            if(ch==')')
+            {
+                //depth--;
+                if(--depth>0)
+                {
+                    ans+=ch;
+                }
+            }
+        }
+        return ans;
+    }
+};
